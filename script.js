47eedb37329c2ed91a7b7fa97a75d88f3df725f8
At41rv1 <at41rv@gmail.com>
2025-06-08 22:20:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ class AIChat {
         // It is strongly recommended to use a backend proxy to handle API requests securely.
         this.apiKey = 'ddc-a4f-796282249bb7466383eaabebc348d027';
         this.baseUrl = 'https://api.a4f.co/v1/chat/completions';
-        this.model = 'provider-4/gpt-4.1';
+        this.model = 'provider-3/gpt-4.5-preview';
         this.conversationHistory = [];
         this.currentUser = null;
         this.autoSave = true;
